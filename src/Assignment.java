4f625abb261a6906e206b7a0f8f2e266c4c13d57
HZ <liuhaochen99@gmail.com>
2021-06-08 09:37:03 +0800

    Initial commit

---
+++ src/Assignment.java
@@ -0,0 +1,35 @@
+import java.util.*;
+//Haochen Liu
+//COMP 251 A2 Q3
+class Assignment implements Comparator<Assignment>{
+	int number;
+	int weight;
+	int deadline;
+	
+	protected Assignment() {
+	}
+	
+	protected Assignment(int number, int weight, int deadline) {
+		this.number = number;
+		this.weight = weight;
+		this.deadline = deadline;
+	}
+	
+	/**
+	 * This method is used to sort to compare assignment objects for sorting. 
+	 */
+	@Override
+	public int compare(Assignment a1, Assignment a2) {
+		// TODO Implement this
+		// implemented as required in description
+		if(a1.weight==a2.weight){
+			return 0;
+		}
+		else if(a1.weight > a2.weight){
+			return -1;
+		}
+		else{
+			return 1;
+		}
+	}
+}
